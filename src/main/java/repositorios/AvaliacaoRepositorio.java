57bf895433ce3c6becb26b0e5b17e07aed6d4ca5
WALISSON ALVES BRAGA <walisson.braga@a.ucb.br>
2026-05-31 02:12:02 -0300

    CLasse AvaliacaoRepositorio

--- src/main/java/repositorios/AvaliacaoRepositorio.java
+++ src/main/java/repositorios/AvaliacaoRepositorio.java
@@ -3,11 +3,77 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package repositorios;
+import classes.Avaliacao;
+import java.util.ArrayList;
 
 /**
  *
  * @author Windows
  */
 public class AvaliacaoRepositorio {
+    
+    //lista de simulacao de bd 
+    private ArrayList<Avaliacao> avaliacoes;
+    
+    //============
+    //Construtor
+    //============
+    
+    public AvaliacaoRepositorio() {
+        avaliacoes = new ArrayList<>();
+    }
+    
+    //===================
+    //Adiciona Avaliacao 
+    //===================
+    
+    //Salva uma nova avaliacao
+    public void adicionar(Avaliacao avaliacao){
+        avaliacoes.add(avaliacao);
+    }
+    
+    //====================
+    //lista as avaliacaoes
+    //====================
+    
+    //Retorna todas as avaliacoes
+    
+    public ArrayList<Avaliacao> listar() {
+        return avaliacoes;
+    }
+    
+    //===============
+    //Busca pelo id 
+    //===============
+    
+    //Busca a avaliacao pelo id
+    public Avaliacao buscaPorID(int idAvaliacao){
+        for (Avaliacao avaliacao : avaliacoes){
+            if (avaliacao.getIdAvaliacao() == idAvaliacao){
+                
+                return avaliacao;
+            }
+        }
+        
+        return null; 
+    }
+    
+    //===================
+    //Remove a avaliacao 
+    //===================
+    
+    //Remove a avaliacao utilizando o id 
+    
+    public boolean remover(int idAvaliacao){
+        Avaliacao avaliacao = buscarPorId(idAvaliacao);
+        
+        if (avaliacao != null){
+            avaliacoes.remove(avaliacao);
+            
+            return true;
+        }
+        
+        return false;
+    }
     
 }
