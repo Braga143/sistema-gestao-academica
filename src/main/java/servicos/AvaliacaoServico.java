c00f9e0335cdbc068cc6a8c31362c0925b6c72f6
WALISSON ALVES BRAGA <walisson.braga@a.ucb.br>
2026-05-31 04:28:41 -0300

    Implementacao do avaliacao servico

--- src/main/java/servicos/AvaliacaoServico.java
+++ src/main/java/servicos/AvaliacaoServico.java
@@ -4,10 +4,91 @@
  */
 package servicos;
 
+import classes.Avaliacao;
+import classes.Matricula;
+
+import repositorios.AvaliacaoRepositorio;
+
+import java.util.ArrayList;
+
+
+
 /**
- *
+ *servico responssavel pelas regras de negocio da avaliacao
  * @author Windows
  */
 public class AvaliacaoServico {
     
+    //repositorio da avaliacao 
+    private AvaliacaoRepositorio avaliacaoRepo;
+    
+    //============
+    //COnstrutor
+    //============
+    public AvaliacaoServico(AvaliacaoRepositorio avaliacaoRepo){
+        this.avaliacaoRepo = avaliacaoRepo;
+    }
+    
+    //Fazer a validacao
+    
+    //cria a avaliacao do aluno
+    public void avaliarAluno(int idAvaliacao, Matricula matricula, double nota, String comentario){
+        
+        //antes de avaliar verifica se ja existe
+        for(Avaliacao avaliacao : avaliacaoRepo.listar()){
+            if(avaliacao.getMatricula().getIdMatricula() == matricula.getIdMatricula()){
+                System.out.println("Aluno ja possui " + "Avaliacao. ");
+                
+                return;
+            }
+        }
+        
+        //Caso a avalaicao nao exista, agora cria a avaliacao
+        Avaliacao avaliacao = new Avaliacao(idAvaliacao, matricula, nota, comentario);
+        
+        //salvar a avaliacao
+        avaliacaoRepo.adicionar(avaliacao);
+        System.out.println("Avaliacao realisada " + "Com Sucesso! ");
+        
+        // Emissao de sertificado de forma automatica
+        if(avaliacao.isCertificadoEmitido()){
+            avaliacao.emitirCertificado();
+        }
+    }
+    
+    //==============
+    //Listar os certificados
+    
+    //o sistema retorna a lista das avaliacoes 
+    
+    public ArrayList <Avaliacao> listarAvaliacaos(){
+        return avaliacaoRepo.listar();
+    }
+    
+    //Busca a avalaiacao pelo id
+    public Avaliacao buscaPorId(int idAvaliacao){
+        
+        return avaliacaoRepo.buscaPorID(idAvaliacao);
+    }
+    
+    //====================
+    //Remocao da avaliacao 
+    //====================
+    
+    //Remove a avaliacao do sistema
+    public boolean removerAvaliacao(int idAvaliacao){
+        Avaliacao avaliacao = avaliacaoRepo.buscaPorID(idAvaliacao);
+        
+        //Caso a avalaicao nao seja encontrata 
+        if(avaliacao == null){
+            System.out.println("Avalaiacao nao encontrada. ");
+            
+            return false;
+        }
+        
+        avaliacaoRepo.remover(idAvaliacao);
+            System.out.println("Avalaicao removida " + "Com Sucesso! ");
+            
+            return true;
+    }
 }
